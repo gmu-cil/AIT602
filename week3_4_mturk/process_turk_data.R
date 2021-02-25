4566fd04f2b032abbd6a0e36a4cf6d535c3973a8
Myeong Lee <deeper.myeong.lee@gmail.com>
2021-02-25 17:10:29 -0500

    example M-turk data analysis

--- week3_4_mturk/process_turk_data.R
+++ week3_4_mturk/process_turk_data.R
@@ -16,8 +16,8 @@ turk2 <- turk2[,c("Input.city_A", "Input.city_B", "Answer.which_city.label")]
 colnames(turk1) <- c("city_A", "city_B", "Answer1")
 colnames(turk2) <- c("city_A", "city_B", "Answer2")
 
-turk1$Answer2 <- NA
 turk2$Answer1 <- NA
+turk1$Answer2 <- NA
 
 turk <- rbind(turk1, turk2)
 rm (turk1)
@@ -26,26 +26,31 @@ turk$city_A <- as.factor(turk$city_A)
 turk$city_B <- as.factor(turk$city_B)
 
 # Counting the Scores
-scores <- as.data.frame(matrix(ncol=2, nrow=6))
-colnames(scores) <- c("city", "score")
+scores <- as.data.frame(matrix(ncol=3, nrow=6))
+colnames(scores) <- c("city", "political_score", "beauty_score")
 scores$city <- unique(c(levels(turk$city_A), levels(turk$city_B)))
-scores$score <- 0
+scores$political_score <- 0
+scores$beauty_score <- 0
 
 for (i in 1:nrow(turk)){
-  if (turk[i,]$Answer1 == "City A"){
-    scores[scores$city == turk[i,]$city_A,]$score <- scores[scores$city == turk[i,]$city_A,]$score + 1
-    next
-  } else {
-    scores[scores$city == turk[i,]$city_B,]$score <- scores[scores$city == turk[i,]$city_B,]$score + 1
-    next
+  if (!is.na(turk[i,]$Answer1)) {
+    if (turk[i,]$Answer1 == "City A"){
+      scores[scores$city == turk[i,]$city_A,]$political_score <- 
+        scores[scores$city == turk[i,]$city_A,]$political_score + 1
+    } else {
+      scores[scores$city == turk[i,]$city_B,]$political_score <- 
+        scores[scores$city == turk[i,]$city_B,]$political_score + 1
+    }
   }
   
-  if (turk[i,]$Answer2 == "City A"){
-    scores[scores$city == turk[i,]$city_A,]$score <- scores[scores$city == turk[i,]$city_A,]$score + 1
-    next
-  } else {
-    scores[scores$city == turk[i,]$city_B,]$score <- scores[scores$city == turk[i,]$city_B,]$score + 1
-    next
+  if (!is.na(turk[i,]$Answer2)) {
+    if (turk[i,]$Answer2 == "City A"){
+      scores[scores$city == turk[i,]$city_A,]$beauty_score <- 
+        scores[scores$city == turk[i,]$city_A,]$beauty_score + 1
+    } else {
+      scores[scores$city == turk[i,]$city_B,]$beauty_score <- 
+        scores[scores$city == turk[i,]$city_B,]$beauty_score + 1
+    }
   }
 }
 
@@ -53,11 +58,12 @@ write.table(scores, "data/turk_scores.csv", row.names=F, col.names=T, sep=",")
 
 
 # Plotting
-summary(lm(score1 ~ score2, data=data))
+summary(lm(political_score ~ beauty_score, data=scores))
 
-ggplot(shannon, aes(x = shannon.entropy, y = score, label = city)) +
-  geom_point(color="blue")  + geom_smooth(method = "lm", se = FALSE, color="red", size=0.2)  +
+ggplot(scores, aes(x = political_score, y = beauty_score, label = city)) +
+  geom_point(color="blue")  + 
+  geom_smooth(method = "lm", se = TRUE, color="red", size=0.2)  +
   theme_bw()+  geom_text(hjust = 0, nudge_x = 0.05) +
-  ylab("Dimension 1")+
-  xlab("Dimension 2") + ggtitle("Relationship between two dimensions of Turker's Perceptions")
+  ylab("Politically Liberal")+
+  xlab("Beautiful") + ggtitle("Relationship between two dimensions of Turker's Perceptions")
 
