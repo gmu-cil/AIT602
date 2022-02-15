e1f61350af727845475d19445bb290f45637e84a
Myeong Lee <deeper.myeong.lee@gmail.com>
2022-02-15 18:42:30 -0500

    week4

--- week3_4_mturk/process_turk_data.R
+++ week3_4_mturk/process_turk_data.R
@@ -4,14 +4,14 @@ library(readr)
 library(dplyr)
 
 
-setwd("~/git/AIT602_Spring2021/week3_4_mturk/")
+setwd("~/git/AIT602/week3_4_mturk/")
 
 # Load the result of M-Turk
-turk1 <- read_delim("data/hingle_turk_hit_file.csv", delim = ",",col_names = TRUE ) 
-turk2 <- read_delim("data/Akshata_BeautifulCity_Results.csv", delim = ",",col_names = TRUE ) 
+turk1 <- read_delim("data/Amani_culturally_diverse_city.csv", delim = ",",col_names = TRUE ) 
+turk2 <- read_delim("data/Krishna_beautiful_city.csv", delim = ",",col_names = TRUE ) 
 
 turk1 <- turk1[,c("Input.city_A", "Input.city_B", "Answer.equal.label")]
-turk2 <- turk2[,c("Input.city_A", "Input.city_B", "Answer.which_city.label")]
+turk2 <- turk2[,c("Input.city_A", "Input.city_B", "Answer.equal.label")]
 
 colnames(turk1) <- c("city_A", "city_B", "Answer1")
 colnames(turk2) <- c("city_A", "city_B", "Answer2")
@@ -27,19 +27,19 @@ turk$city_B <- as.factor(turk$city_B)
 
 # Counting the Scores
 scores <- as.data.frame(matrix(ncol=3, nrow=6))
-colnames(scores) <- c("city", "political_score", "beauty_score")
+colnames(scores) <- c("city", "cultural_score", "beauty_score")
 scores$city <- unique(c(levels(turk$city_A), levels(turk$city_B)))
-scores$political_score <- 0
+scores$cultural_score <- 0
 scores$beauty_score <- 0
 
 for (i in 1:nrow(turk)){
   if (!is.na(turk[i,]$Answer1)) {
     if (turk[i,]$Answer1 == "City A"){
-      scores[scores$city == turk[i,]$city_A,]$political_score <- 
-        scores[scores$city == turk[i,]$city_A,]$political_score + 1
+      scores[scores$city == turk[i,]$city_A,]$cultural_score <- 
+        scores[scores$city == turk[i,]$city_A,]$cultural_score + 1
     } else {
-      scores[scores$city == turk[i,]$city_B,]$political_score <- 
-        scores[scores$city == turk[i,]$city_B,]$political_score + 1
+      scores[scores$city == turk[i,]$city_B,]$cultural_score <- 
+        scores[scores$city == turk[i,]$city_B,]$cultural_score + 1
     }
   }
   
@@ -58,12 +58,12 @@ write.table(scores, "data/turk_scores.csv", row.names=F, col.names=T, sep=",")
 
 
 # Plotting
-summary(lm(political_score ~ beauty_score, data=scores))
+summary(lm(cultural_score ~ beauty_score, data=scores))
 
-ggplot(scores, aes(x = political_score, y = beauty_score, label = city)) +
+ggplot(scores, aes(x = cultural_score, y = beauty_score, label = city)) +
   geom_point(color="blue")  + 
   geom_smooth(method = "lm", se = TRUE, color="red", size=0.2)  +
   theme_bw()+  geom_text(hjust = 0, nudge_x = 0.05) +
-  ylab("Politically Liberal")+
+  ylab("Culturally diverse")+
   xlab("Beautiful") + ggtitle("Relationship between two dimensions of Turker's Perceptions")
 
